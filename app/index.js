e1f91bb9fcad3a1a94b6ac63d9cb4d91a5bbc181
Viliam Rockai <vrockai@redhat.com>
2014-12-10 17:28:16 +0100

    dot file fix

--- app/index.js
+++ app/index.js
@@ -66,7 +66,7 @@ var LiveoakGenerator = yeoman.generators.Base.extend({
       this.template('_package.json', 'package.json');
       this.template('_application.json', 'application.json');
 
-      this.src.copy('.gitignore', 'app/templates/.gitignore');
+      this.src.copy('.gitignore', 'app/templates/_gitignore');
     },
 
     projectfiles: function () {
